95026fd3a5ae99da52d8d897163f62a53a59cbdf
coincheung <867153576@qq.com>
2019-04-19 09:29:31 +0800

    triplet loss

---
+++ triplet_loss.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+
+import torch
+import torch.nn as nn
+
+
+class TripletLoss(nn.Module):
+    '''
+    Compute normal triplet loss or soft margin triplet loss given triplets
+    '''
+    def __init__(self, margin=None):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        if self.margin is None:  # if no margin assigned, use soft-margin
+            self.Loss = nn.SoftMarginLoss()
+        else:
+            self.Loss = nn.TripletMarginLoss(margin=margin, p=2)
+
+    def forward(self, anchor, pos, neg):
+        if self.margin is None:
+            num_samples = anchor.shape[0]
+            y = torch.ones((num_samples, 1)).view(-1)
+            if anchor.is_cuda: y = y.cuda()
+            ap_dist = torch.norm(anchor-pos, 2, dim=1).view(-1)
+            an_dist = torch.norm(anchor-neg, 2, dim=1).view(-1)
+            loss = self.Loss(an_dist - ap_dist, y)
+        else:
+            loss = self.Loss(anchor, pos, neg)
+
+        return loss
+
+
+if __name__ == '__main__':
+    pass
